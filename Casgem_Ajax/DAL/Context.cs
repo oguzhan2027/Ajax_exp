f104fb8f41c969d130cccf0f3ef215300533d734
oguzhan2027 <oguzhancelik2027@gmail.com>
2023-07-31 12:15:57 +0300

    ...

---
+++ Casgem_Ajax/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Casgem_Ajax.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-4UM3KTM;initial catalog=CasgemAjaxDb;integrated Security=true");
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
